b7a643fdc5099fa4bf66902a4a320040c3629cc4
Linwood <Linwood93@gmail.com>
2021-09-02 21:03:03 -0400

    another one basic date stuff

--- Day001.java
+++ Day001.java
@@ -1,12 +1,13 @@
 import java.security.SecureRandom;
 
+
 public final class Day001 {
 
     public static final SecureRandom SECURE_RANDOM = new SecureRandom();
 
     public static void main(String[] args) {
-        System.out.println("Generating a number between 50 and 100...");
-        System.out.println(randomNumberBetween(50, 100));
+        System.out.println("Generating a number between 1 and 1000...");
+        System.out.println(randomNumberBetween(1, 1000));
     }
 
     private static int randomNumberBetween(int minimum, int maximum) {
